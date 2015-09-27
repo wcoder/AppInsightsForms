fea3e1861e4e534fb605ff9b2f2ac618144ced53
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-09-27 16:10:44 +0300

    Initial commit

---
+++ ApplicationInsightsXFormsDemo/ApplicationInsightsXFormsDemo.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationInsightsXFormsDemo.WinPhone.Resources;
+
+namespace ApplicationInsightsXFormsDemo.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
